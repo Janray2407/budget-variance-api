5f38fad20492607e26778ca01a2262a38d53dba7
Janray2407 <janray2407@gmail.com>
2026-03-31 12:49:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,84 @@
 const express = require("express");
-const multer = require("multer");
-const csv = require("csv-parser");
-const fs = require("fs");
-const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
 
-const upload = multer({ dest: "uploads/" });
+const uri = "YOUR_MONGODB_CONNECTION_STRING";
 
-const uri = "YOUR_MONGODB_CONNECTION";
 const client = new MongoClient(uri);
 
-async function uploadCSV(path, collectionName){
-
+async function connectDB() {
 await client.connect();
-
-const db = client.db("budget_system");
-const collection = db.collection(collectionName);
-
-const results = [];
-
-fs.createReadStream(path)
-.pipe(csv())
-.on("data",(data)=> results.push(data))
-.on("end", async ()=>{
-
-await collection.insertMany(results);
-
-console.log("Upload Done");
-
-});
-
 }
 
-app.post("/upload-budget", upload.single("file"), async (req,res)=>{
+connectDB();
 
-await uploadCSV(req.file.path,"budget");
+const db = client.db("budget_monitoring");
+
+app.post("/uploadBudget", async (req,res)=>{
+
+const data = req.body;
+
+await db.collection("budget_data").insertMany(data);
 
 res.send("Budget Uploaded");
 
 });
 
-app.post("/upload-actual", upload.single("file"), async (req,res)=>{
+app.post("/uploadActual", async (req,res)=>{
 
-await uploadCSV(req.file.path,"actual");
+const data = req.body;
+
+await db.collection("actual_data").insertMany(data);
 
 res.send("Actual Uploaded");
 
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/calculateVariance", async (req,res)=>{
 
-app.listen(PORT, ()=>{
-console.log("Server Running");
+const actual = await db.collection("actual_data").find().toArray();
+const budget = await db.collection("budget_data").find().toArray();
+
+let result = [];
+
+actual.forEach(a=>{
+
+const b = budget.find(x=>x.account === a.account);
+
+if(b){
+
+const variance = a.net - b.budget;
+
+result.push({
+account:a.account,
+actual:a.net,
+budget:b.budget,
+variance:variance
+});
+
+}
+
+});
+
+await db.collection("variance_result").insertMany(result);
+
+res.json(result);
+
+});
+
+app.get("/dashboard", async(req,res)=>{
+
+const data = await db.collection("variance_result").find().toArray();
+
+res.json(data);
+
+});
+
+app.listen(3000,()=>{
+console.log("Server running");
 });
